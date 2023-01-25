aea6656eb9226e8cd34c20632d15357f7347a69e
Kaung Khant Si Thu <76657068+KaungKhantSiThu9@users.noreply.github.com>
2023-01-25 16:06:29 +0700

    Added blog page

---
+++ .cache/sync-requires.js
@@ -0,0 +1,23 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/about.js")),
+  "component---src-pages-blog-edit-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/blog/edit.js")),
+  "component---src-pages-blog-index-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/blog/index.js")),
+  "component---src-pages-blog-new-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/blog/new.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/index.js")),
+  "component---src-pages-page-1-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/page1.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/page-2.js")),
+  "component---src-pages-pos-index-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/pos/index.js")),
+  "component---src-pages-prev-wine-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/prevWine.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/using-typescript.tsx")),
+  "component---src-pages-wine-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/pages/wine.js")),
+  "component---src-templates-using-dsg-js": preferDefault(require("/Users/kaungkhantsithu/stock-mgnt-gatsby/src/templates/using-dsg.js"))
+}
+
